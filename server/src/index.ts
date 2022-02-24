ef7abca00ed8e37a1da89a40e0ac842b5c986380
Muhammad Zubair <muhzubair321@gmail.com>
2022-02-24 14:56:40 +0500

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors from 'cors'
+import { createConnection } from "typeorm";
+import {schema} from './Schema'
+
+const main = async() =>{
+    await createConnection({
+        type:"mysql",
+        database:"graphqlcrud",
+        username:"root",
+        password:"",
+        logging:true,
+        synchronize:false,
+        entities:[],
+    })
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use("/graphql",graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+app.listen(3001,()=>{
+    console.log("running on 3001");
+    
+})
+}
+
+main().catch((err)=>{
+    console.log(err);
+    
+})
